d40e69274a14e28a05bdc5edcfe5b07c5b8492f9
Poulami-Nandi <164754016+Poulami-Nandi@users.noreply.github.com>
2025-01-13 12:38:41 +0100

    Update test_face_recognition.py

--- tests/test_face_recognition.py
+++ tests/test_face_recognition.py
@@ -1,9 +1,34 @@
 import unittest
+import cv2
+import face_recognition
+import numpy as np
+from PIL import Image, ImageDraw
+import matplotlib.pyplot as plt
+import numpy as np
 from utils.face_recognition import find_face_locations_and_encodings
 
 class TestFaceRecognition(unittest.TestCase):
     def test_recognize_face(self):
         face_locations, face_encodings = find_face_locations_and_encodings('images/human_face.jpg')
+        print("Number of faces detected:", len(face_locations))
+        print("\nOriginal image")
+        display(Image.open(image_path))
+        # 1. Display Face Locations
+        image = face_recognition.load_image_file(image_path)  
+        pil_image = Image.fromarray(image)
+        draw = ImageDraw.Draw(pil_image)
+
+        for (top, right, bottom, left) in face_locations:
+          draw.rectangle(((left, top), (right, bottom)), outline="red", width=2)
+        print("\nFace location in original image")
+        display(pil_image) 
+
+        print("\nFace encoding")
+        # 2. Display Face Encodings (as grayscale images)
+        for encoding in face_encodings:
+          encoding_image = encoding.reshape(8, 16)  # Adjust dimensions if needed
+          plt.imshow(encoding_image, cmap="gray")
+          plt.show()
 
 if __name__ == '__main__':
     unittest.main()
