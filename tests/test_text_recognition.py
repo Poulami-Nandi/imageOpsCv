d2d35766384673491c456207088993355897940f
Poulami-Nandi <164754016+Poulami-Nandi@users.noreply.github.com>
2025-01-14 11:26:54 +0100

    Update test_text_recognition.py

--- tests/test_text_recognition.py
+++ tests/test_text_recognition.py
@@ -3,7 +3,7 @@ from utils.text_recognition import recognize_text
 
 class TestTextRecognition(unittest.TestCase):
     def test_recognize_text(self):
-        text = recognize_text('images/handwritten_note.jpg')
+        text = recognize_text('images/handwritten_note.PNG')
         self.assertIsInstance(text, str)
         self.assertGreater(len(text), 0)
         print(text)
